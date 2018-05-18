f9825e71b5ebc9e50c3b9a4ca3554ab7c70168c1
Frank Quinn <fquinn@cascadium.io>
2018-05-18 19:59:07 +0100

    Nunit console

--- release_scripts/ci-run.py
+++ release_scripts/ci-run.py
@@ -167,6 +167,7 @@ run_command(args=["java",
 nunit_console = "C:\\Program Files (x86)\\NUnit 2.6.4\\bin\\nunit-console.exe"
 if not os.path.exists(nunit_console):
     nunit_console = "nunit3-console"
+print ("NUNIT_CONSOLE: " + nunit_console)
 
 if os.name == "nt":
     env_var["middlewareName"] = middleware
